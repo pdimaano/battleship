3ad743b8b71d74441445427c010f4bf63bf67969
Philip Dimaano <pdimaano7@gmail.com>
2022-03-10 16:30:03 -0800

    included ship data

---
+++ src/data/shipData.js
@@ -0,0 +1,49 @@
+const shipData = [
+    {
+        size: 5,
+        shipId: "Carrier",
+        className: "Carrier",
+        key: 0,
+        coords: [],
+        health: 5,
+        status: "deployed"
+    },
+    {
+        size: 4,
+        shipId: "Battleship",
+        className: "Battleship",
+        key: 1,
+        coords: [],
+        health: 4,
+        status: "deployed"
+    },
+    {
+        size: 3,
+        shipId: "Destroyer",
+        className: "Destroyer",
+        key: 2,
+        coords: [],
+        health: 3,
+        status: "deployed"
+    },
+    {
+        size: 3,
+        shipId: "Submarine",
+        className: "Submarine",
+        key: 3,
+        coords: [],
+        health: 3,
+        status: "deployed"
+    },
+    {
+        size: 2,
+        shipId: "Patrol Boat",
+        className: "Patrol Boat",
+        key: 4,
+        coords: [],
+        health: 2,
+        status: "deployed"
+    },
+]
+
+export default shipData;
