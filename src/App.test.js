3458edbd1c2f8625a86811874842d4e66cc471b8
Philip Dimaano <pdimaano7@gmail.com>
2022-03-11 13:32:53 -0800

    initial css

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const AppFunction = App();
+
+test('testing', () => {
+  expect(AppFunction.getTargets(
+    [0]
+  )).toEqual( [1, 10] )});
